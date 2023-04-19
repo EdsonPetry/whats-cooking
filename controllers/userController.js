68a533df3ad42c67641657d380e1c20a0beedeae
Edson Petry <edsonlbpetry@gmail.com>
2023-04-19 12:01:22 -0400

    threadControllers.js set up, connection.js fixed, authController.js typo fixed

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,24 +1,20 @@
-const router = require('express').Router();
-const User = require('../models/user')
+const router = require("express").Router();
+const User = require("../models/user");
 
-// function isAuthenticated(req, res, next) {
-//   if (!req.session.user_id) {
-//     return res.redirect('/login')
-//   };
-  
-//   next();
-// }
+function isAuthenticated(req, res, next) {
+  if (!req.session.user_id) {
+    return res.redirect("/login");
+  }
 
-// router.get('/dashboard', isAuthenticated, async (req, res) => {
-//   const user = await User.findbyPK(req.session.user_id)
-//     res.render('user_dashboard', {
-//       email: user.email
-//     });
-//   });
+  next();
+}
 
-  router.get('/dashboard', async (req, res) => {
-    
-      res.render('user_dashboard');
-    });
+router.get("/dashboard", isAuthenticated, async (req, res) => {
+  const user = await User.findByPK(req.session.user_id); //corrected 'findbyPK' to 'findByPK'
+  res.render("user_dashboard", {
+    email: user.email,
+  });
+});
 
+//removed unecessary route '/dashboard'
 module.exports = router;
