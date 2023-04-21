573d887ad4d498efa26e3b3bbe5ecef500e8af46
AuTangen <atangen2406@gmail.com>
2023-04-21 11:37:30 -0400

    polished UI/added links to dashboard,logout

--- controllers/publicController.js
+++ controllers/publicController.js
@@ -1,11 +1,14 @@
 const router = require("express").Router();
 const Post = require('../models/post')
+const User = require('../models/user')
 const axios = require('axios');
 
 router.get("/", async (req, res) => {
   const post = await Post.findAll({raw: true});
+  const user = await User.findByPk(req.session.user_id)
   res.render("index", {
-    post: post
+    post: post,
+    user: user
  });
 });
 
